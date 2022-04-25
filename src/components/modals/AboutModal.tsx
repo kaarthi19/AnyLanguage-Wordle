a5c2765e64364e051814dd472762ed4ee7c6044c
Kaarthigeswaran A Gnapathy <63862946+kaarthi19@users.noreply.github.com>
2022-04-25 15:11:47 -0400

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -22,41 +22,6 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
             {CONFIG.author}
           </a>{' '}
         </Trans>
-        <Trans i18nKey="aboutCodeSentence">
-          Check out
-          <a
-            href="https://github.com/hannahcode/wordle"
-            className="underline font-bold"
-          >
-            the original code
-          </a>
-          by
-          <a
-            href="https://www.hannahmariepark.com/"
-            className="underline font-bold"
-          >
-            Hannah Park
-          </a>
-          or have a look at
-          <a
-            href="https://github.com/roedoejet/AnyLanguage-Wordle"
-            className="underline font-bold"
-          >
-            Aidan Pine's fork
-          </a>
-          and customize it for another language!
-        </Trans>
-        <Trans
-          i18nKey="aboutDataSentence"
-          values={{ wordListSource: CONFIG.wordListSource }}
-        >
-          The words for this game were sourced from
-          <a href={CONFIG.wordListSourceLink} className="underline font-bold">
-            {CONFIG.wordListSource}
-          </a>
-          .
-        </Trans>
-        <Trans i18nKey="aboutOriginalSentence">
           You can also
           <a
             href="https://www.powerlanguage.co.uk/wordle/"
